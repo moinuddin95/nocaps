6567d5b5ffffad15233a41f26f95f82e332b7d62
moinuddin95 <moinuddinshaikh173@gmail.com>
2025-08-18 22:32:37 -0400

    CLI security flow completed

--- nocaps/authorization.py
+++ nocaps/authorization.py
@@ -12,6 +12,30 @@ SERVICE_NAME = "mycli-auth0"   # identifier for keyring storage
 ACCESS_TOKEN_KEY = "access_token"
 REFRESH_TOKEN_KEY = "refresh_token"
 
+def save_tokens(access_token, refresh_token = None):
+    keyring.set_password(SERVICE_NAME, ACCESS_TOKEN_KEY, access_token)
+    if refresh_token:
+        keyring.set_password(SERVICE_NAME, REFRESH_TOKEN_KEY, refresh_token)
+
+def load_tokens():
+    access_token = keyring.get_password(SERVICE_NAME, ACCESS_TOKEN_KEY)
+    refresh_token = keyring.get_password(SERVICE_NAME, REFRESH_TOKEN_KEY)
+    return access_token, refresh_token
+
+def refresh_access_token(refresh_token):
+    url = f"https://{AUTH0_DOMAIN}/oauth/token"
+    data = {
+        "client_id": CLIENT_ID,
+        "grant_type": "refresh_token",
+        "refresh_token": refresh_token
+    }
+    resp = requests.post(url, data=data).json()
+    if "access_token" in resp:
+        save_tokens(resp["access_token"])
+        return resp["access_token"]
+    else:
+        raise Exception(f"Failed to refresh token: {resp}")
+
 def request_user_code():
     url = f"https://{AUTH0_DOMAIN}/oauth/device/code"
     data = {
@@ -20,7 +44,10 @@ def request_user_code():
         "audience": AUDIENCE
     }
     resp = requests.post(url, data=data).json()
-    
+
+    if "error" in resp:
+        raise Exception(f"Failed to request user code: {resp}")
+
     device_code = resp["device_code"]
     user_code = resp["user_code"]
     verification_uri_complete = resp["verification_uri_complete"]
@@ -52,14 +79,26 @@ def poll_for_tokens(device_code, interval):
                 raise Exception(f"Auth failed: {token_resp}")
         else:
             print("✅ Auth successful!")
+            save_tokens(token_resp["access_token"], token_resp.get("refresh_token"))
             return token_resp  # contains access_token, refresh_token (if requested)
 
 def device_authorization():
-    
-  device_code, interval = request_user_code()
-  tokens = poll_for_tokens(device_code, interval)
-  return tokens
+  access_token, refresh_token = load_tokens()
+  if access_token:
+    print("Access token found!")
+  elif refresh_token:
+    print("Refresh token found! Refreshing...")
+    save_tokens(refresh_access_token(refresh_token))
+  else:
+    print("No tokens found. Starting device authorization flow...")
+    device_code, interval = request_user_code()
+    tokens = poll_for_tokens(device_code, interval)
+    return tokens
 
 if __name__ == "__main__":
+    
+    # keyring.delete_password(SERVICE_NAME, ACCESS_TOKEN_KEY)
+    # keyring.delete_password(SERVICE_NAME, REFRESH_TOKEN_KEY)
+
     tokens = device_authorization()
     print(tokens)
